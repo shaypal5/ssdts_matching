14ea18eebf0be4da3f72ce9616864bff03e2ca91
Shay Palachy <shaypal5@gmail.com>
2017-07-21 14:39:13 +0300

    added versioneer

--- ssdts_matching/__init__.py
+++ ssdts_matching/__init__.py
@@ -8,3 +8,7 @@ from .core import (
     vertical_aligned_timestamp_match,
     delta_partitioned_timestamp_match
 )
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
