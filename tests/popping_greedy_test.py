27b178a0d1f67fddf772b5c07ead0fc92799822e
Shay Palachy <shaypal5@gmail.com>
2017-11-09 13:06:59 +0200

    testing now works

--- tests/popping_greedy_test.py
+++ tests/popping_greedy_test.py
@@ -1,9 +1,27 @@
 """Test the popping_greedy_timestamp_match function."""
 
-# import pytest
-# with pytest.raises(ValueError):
+import ssdts_matching as ssdts
+# from ssdts_matching import popping_greedy_timestamp_match as popping
 
-from ssdts_matching import popping_greedy_timestamp_match as popping
+from .shared import (
+    SHORT_SERIES_1,
+    SHORT_SERIES_2,
+)
+
 
 def test_popping_1():
     """Test 1 for the popping_greedy_timestamp_match function."""
+    res1 = ssdts.popping_greedy_timestamp_match(
+        SHORT_SERIES_1, SHORT_SERIES_2, 1)
+    assert len(res1) == 2
+    assert res1[3] == 3
+    assert res1[10] == 10
+
+    res2 = ssdts.popping_greedy_timestamp_match(
+        SHORT_SERIES_1, SHORT_SERIES_2, 2)
+    assert len(res2) == 5
+    assert res2[1] == 2
+    assert res2[3] == 3
+    assert res2[4] == 5
+    assert res2[8] == 7
+    assert res2[10] == 10
--- tests/shared.py
+++ tests/shared.py
@@ -1,4 +1,6 @@
 """Resources shared by tests of the ssdts_matching package."""
 
-SHORT_SERIES_1 = [1, 3, 4, 8, 10]
-SHORT_SERIES_2 = [2, 3, 5, 6, 7, 10, 11]
+from sortedcontainers import SortedList
+
+SHORT_SERIES_1 = SortedList([1, 3, 4, 8, 10])
+SHORT_SERIES_2 = SortedList([2, 3, 5, 6, 7, 10, 11])
